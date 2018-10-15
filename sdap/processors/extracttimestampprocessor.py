f19bd16cb67d7c0d9942628ca1db9bd942246c95
Yam <elizabeth.yam@jpl.nasa.gov>
2018-10-15 14:33:01 -0700

    remove unused vars and check for tile_type

--- sdap/processors/extracttimestampprocessor.py
+++ sdap/processors/extracttimestampprocessor.py
@@ -19,6 +19,7 @@ from nexusproto.serialization import from_shaped_array
 import datetime
 import time
 import logging
+import numpy
 from netCDF4 import Dataset, num2date
 from pytz import timezone
 
@@ -26,6 +27,9 @@ from sdap.processors import NexusTileProcessor
 
 EPOCH = timezone('UTC').localize(datetime.datetime(1970, 1, 1))
 
+
+class BadTimestampExtractionException(Exception):
+    pass
 
 def to_seconds_from_epoch(timestamp, pattern='%Y-%m-%dT%H:%M:%S'):
     try:
@@ -39,7 +43,6 @@ class ExtractTimestampProcessor(NexusTileProcessor):
     def __init__(self, attribute_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.stored_var_name = self.environ['STORED_VAR_NAME']
         self.attribute_name = attribute_name
 
     def process_nexus_tile(self, nexus_tile):
@@ -49,15 +52,15 @@ class ExtractTimestampProcessor(NexusTileProcessor):
         file_path = output_tile.summary.granule
         file_path = file_path[len('file:'):] if file_path.startswith('file:') else file_path
 
-        dimtoslice = {}
-        for dimension in output_tile.summary.section_spec.split(','):
-            name, start, stop = dimension.split(':')
-            dimtoslice[name] = slice(int(start), int(stop))
+        tile_type = nexus_tile.tile.WhichOneof("tile_type")
 
         with Dataset(file_path) as ds:
-            # MODIS: time_coverage_start
             timestamp = getattr(ds,self.attribute_name)
             seconds = to_seconds_from_epoch(timestamp)
-            nexus_tile.tile.grid_tile.time = seconds
+
+            if tile_type == "grid_tile":
+                nexus_tile.tile.grid_tile.time = seconds
+            else:
+                raise BadTimestampExtractionException
 
         yield nexus_tile
--- tests/extracttimestamp_test.py
+++ tests/extracttimestamp_test.py
@@ -18,13 +18,14 @@ import logging
 
 import sdap.processors
 from nexusproto import DataTile_pb2 as nexusproto
+from sdap.processors.extracttimestampprocessor import BadTimestampExtractionException
 
 
 class TestExtractTimestamp(unittest.TestCase):
     def setUp(self):
         self.module = sdap.processors.ExtractTimestampProcessor('time_coverage_start')
 
-    def test_read_not_empty_wswm(self):
+    def test_extract_timestamp_from_metadata(self):
         test_file = path.join(path.dirname(__file__), 'datafiles', 'not_empty_modis.nc')
 
         input_tile = nexusproto.NexusTile()
@@ -33,11 +34,39 @@ class TestExtractTimestamp(unittest.TestCase):
         tile_summary.section_spec = "time:0:1,lat:0:10,lon:0:10"
         input_tile.summary.CopyFrom(tile_summary)
 
+        input_tile.tile.grid_tile.CopyFrom(nexusproto.GridTile())
         results = list(self.module.process_nexus_tile(input_tile))
-
         nexus_tile_after = results[0]
 
         self.assertEqual(1537428301, nexus_tile_after.tile.grid_tile.time)
+
+    def test_extract_timestamp_swath_exception(self):
+        test_file = path.join(path.dirname(__file__), 'datafiles', 'not_empty_modis.nc')
+
+        input_tile = nexusproto.NexusTile()
+        tile_summary = nexusproto.TileSummary()
+        tile_summary.granule = "file:%s" % test_file
+        tile_summary.section_spec = "time:0:1,lat:0:10,lon:0:10"
+        input_tile.summary.CopyFrom(tile_summary)
+
+        input_tile.tile.swath_tile.CopyFrom(nexusproto.SwathTile())
+
+        with self.assertRaises(BadTimestampExtractionException):
+            list(self.module.process_nexus_tile(input_tile))[0].tile.swath_tile.time
+
+    def test_extract_timestamp_timeseries_exception(self):
+        test_file = path.join(path.dirname(__file__), 'datafiles', 'not_empty_modis.nc')
+
+        input_tile = nexusproto.NexusTile()
+        tile_summary = nexusproto.TileSummary()
+        tile_summary.granule = "file:%s" % test_file
+        tile_summary.section_spec = "time:0:1,lat:0:10,lon:0:10"
+        input_tile.summary.CopyFrom(tile_summary)
+
+        input_tile.tile.time_series_tile.CopyFrom(nexusproto.TimeSeriesTile())
+
+        with self.assertRaises(BadTimestampExtractionException):
+            list(self.module.process_nexus_tile(input_tile))[0].tile.time_series_tile.time
 
 if __name__ == '__main__':
     unittest.main()
